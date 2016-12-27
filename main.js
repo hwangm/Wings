db746463a3df08d271b09f65cc88f989e0d3eaf1
mhwang <mahwang@ucdavis.edu>
2016-12-27 15:46:00 -0800

    add moment for server side jade time support. set default value of date input to current date, and prevent user from entering dates prior to current date

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+//init moment for time support in jade
+app.locals.moment = require('moment');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
